29a711f15ab92329d1f44880654ac122ae7d94e6
dashakiska69 <dashakiska69@gmail.com>
2019-12-11 13:11:29 +0300

    first commit

---
+++ SeleniumDemo/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace SeleniumDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button2 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(376, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(176, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Закрыть браузер";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(195, 12);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(175, 23);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Открыть браузер";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(195, 41);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(357, 76);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "Вывести список квартир";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 123);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(735, 506);
+            this.dataGridView1.TabIndex = 10;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Название";
+            this.Column1.Name = "Column1";
+            this.Column1.Width = 230;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Цена";
+            this.Column2.Name = "Column2";
+            this.Column2.Width = 230;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Адресс";
+            this.Column3.Name = "Column3";
+            this.Column3.Width = 230;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(756, 641);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button2);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+    }
+}
+
---
+++ SeleniumDemo/Form1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace SeleniumDemo
+{
+    public partial class Form1 : Form
+    {
+        IWebDriver Browser;
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            Browser.Navigate().GoToUrl("http://google.com");
+
+            IWebElement SearchInput = Browser.FindElement(By.Name("q"));
+            SearchInput.SendKeys("Как вырастить гомункула?" + OpenQA.Selenium.Keys.Enter);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Browser.Quit();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Browser = new OpenQA.Selenium.Chrome.ChromeDriver();
+            Browser.Manage().Window.Maximize();
+            Browser.Navigate().GoToUrl("https://www.avito.ru/kirovskaya_oblast_kirov/kvartiry");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            IWebElement element;
+            //Поиск по id элемента
+            //element= Browser.FindElement(By.Id("text"));
+            //element.SendKeys("Tect");
+
+            //Поиск по классу элемента
+            //element = Browser.FindElement(By.ClassName("home-logo__default"));
+            //element.Click();
+
+            //Поиск по тексту ссылки
+            //element = Browser.FindElement(By.LinkText("Картинки"));
+            //element.Click();
+
+            //Поиск по частичному тексту ссылки
+            element = Browser.FindElement(By.PartialLinkText("ревод"));
+            element.Click();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            //List<IWebElement> News = Browser.FindElements(By.XPath(".//*[@id='catalog']/div[5]/div[1]/div[4]/div[2]/div[3]/div/div[2]/div/div[2]/div[1]/div[1]/h3/a/span")).ToList();
+            //List<IWebElement> News = Browser.FindElements(By.XPath("html/body/div[5]/div[1]/div[4]/div[2]/div[3]/div/div[2]/div/div[2]/div[1]/div[1]/h3/a/span")).ToList();
+
+
+            List<IWebElement> Name = Browser.FindElements(By.XPath("//a[@class='item-description-title-link']/span")).ToList();
+            List<IWebElement> Price = Browser.FindElements(By.XPath("//span[@itemprop='price']")).ToList();
+            List<IWebElement> Adress = Browser.FindElements(By.XPath("//span[@class='item-address__string']")).ToList();
+
+           // List<IWebElement> Price = Browser.FindElements(By.XPath("//div[@class='item_table-description']/div[1]/span[2]]")).ToList();
+          //  List<IWebElement> Name = Browser.FindElements(By.XPath("//div[@class='description item_table-description']/div[1]/h3/a/span")).ToList();
+           // List<IWebElement> Adress = Browser.FindElements(By.XPath("//div[@class='description item_table-description']/div[2]/div[1]/span[3]")).ToList();
+            for (int i = 0; i < Name.Count; i++)
+            {
+                    
+                
+                //Выведение новости
+                //textBox1.AppendText(Price[i].Text + "\r\n");
+              //  textBox1.AppendText(Name[i].Text + "\r\n");
+                dataGridView1.Rows.Add(Name[i].Text, Price[i].Text, Adress[i].Text);
+               
+                
+               
+
+               // String s = News[i].Text;
+
+              /*  if (s.StartsWith("Москва"))
+                {
+                    textBox1.AppendText("Новость № " + (i + 1).ToString() + " Начинается с текста 'Москва'");
+
+                }
+
+                if (s.EndsWith("границы"))
+                {
+                    textBox1.AppendText("Новость № " + (i + 1).ToString() + " заканчивается на слово 'границы'");
+
+                }
+
+                if (s.Contains("МИД"))
+                {
+                    textBox1.AppendText("Новость № " + (i + 1).ToString() + " заканчивается на слово 'МИД");
+                    News[i].Click();
+                    break;
+                }*/
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            IJavaScriptExecutor jse = Browser as IJavaScriptExecutor;
+            jse.ExecuteScript("alert('test');");
+        }
+
+        private string FindWindow(String Url)
+        {
+            String StartWindow = Browser.CurrentWindowHandle;
+            String Result = "";
+
+            for (int i = 0; i < Browser.WindowHandles.Count;i++)
+            { if (Browser.WindowHandles[i] != StartWindow)
+                {
+                    Browser.SwitchTo().Window(Browser.WindowHandles[i]);
+                    if(Browser.Url.Contains(Url))
+                    {
+                        Result = Browser.WindowHandles[i];
+                        break;
+                    }
+                }
+            }
+            Browser.SwitchTo().Window(StartWindow);
+            return Result;
+        }
+        private void button7_Click(object sender, EventArgs e)
+        {
+            /* Browser.SwitchTo().Window(Browser.WindowHandles[1]);
+             System.Windows.Forms.MessageBox.Show(Browser.Title + "\r\n" + Browser.Url);
+
+             Browser.SwitchTo().Window(Browser.WindowHandles[0]);
+             System.Windows.Forms.MessageBox.Show(Browser.Title + "\r\n" + Browser.Url);
+
+             Browser.SwitchTo().Window(Browser.WindowHandles[2]);
+             System.Windows.Forms.MessageBox.Show(Browser.Title + "\r\n" + Browser.Url);
+
+            String HabrWindow = FindWindow("habr");
+            Browser.SwitchTo().Window(HabrWindow);
+            System.Windows.Forms.MessageBox.Show(Browser.Title + "\r\n" + Browser.Url);*/
+
+            
+        }
+
+
+
+
+       
+
+
+    }
+}
